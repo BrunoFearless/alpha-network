61cd4b8d971d7483b08bda61aa7517e78ffb28e9
Caleb Figueiredo <calebfigueiredo28@gmail.com>
2026-04-16 00:49:04 +0100

    feat(auth): trata exceção de conta deletada no filtro global

--- apps/api/src/common/filters/http-exception.filter.ts
+++ apps/api/src/common/filters/http-exception.filter.ts
@@ -7,15 +7,14 @@ import { Request, Response } from 'express';
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx    = host.switchToHttp();
-    const res    = ctx.getResponse<Response>();
-    const req    = ctx.getRequest<Request>();
+    const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
+    const req = ctx.getRequest<Request>();
     const status = exception.getStatus();
     const excRes = exception.getResponse();
-    const message =
-      typeof excRes === 'object' && 'message' in (excRes as object)
-        ? (excRes as any).message
-        : exception.message;
+
+    const extraData = typeof excRes === 'object' ? (excRes as any) : {}
+    const message = extraData.message || exception.message
 
     res.status(status).json({
       success: false,
@@ -24,6 +23,7 @@ export class HttpExceptionFilter implements ExceptionFilter {
         message: Array.isArray(message) ? message[0] : message,
         path: req.url,
         timestamp: new Date().toISOString(),
+        ...extraData,
       },
     });
   }
