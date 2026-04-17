4603241fd023ad604043f9bddd816b30f3f3ba35
Caleb Figueiredo <calebfigueiredo28@gmail.com>
2026-04-17 18:33:16 +0100

    feat(users): adiciona sistema de seguimento completo

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,7 +1,7 @@
 import {
   Controller, Get, Patch, Delete, Post,
   Body, Param, UseGuards, HttpCode, HttpStatus,
-  UploadedFile, UseInterceptors, BadRequestException,
+  UploadedFile, UseInterceptors, BadRequestException, Req,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { UsersService } from './users.service';
@@ -9,6 +9,7 @@ import { JwtAuthGuard } from '../common/guards/jwt-auth.guard';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { UpdateModesDto } from './dto/update-modes.dto';
 import { UpdateProfileDto } from './dto/update-profile.dto';
+import { OptionalJwtAuthGuard } from '@auth/guards/Optional-jwt-auth.guard';
 
 @Controller('users')
 export class UsersController {
@@ -24,15 +25,19 @@ export class UsersController {
     return { success: true, data: { ...user.profile, id: user.id, user: safeUser } };
   }
 
+  // ── Perfil público ─────────────────────────────────────────────────
   @Get(':username')
-  async getProfile(@Param('username') username: string) {
-    const profile = await this.usersService.findByUsername(username);
-    if (!profile) {
-      return { success: false, error: { message: 'Utilizador não encontrado.' } };
-    }
-    const { user } = profile;
-    const { passwordHash, deletedAt, emailVerified, ...safeUser } = user as any;
-    return { success: true, data: { ...profile, id: profile.userId, user: safeUser } };
+  @UseGuards(OptionalJwtAuthGuard)
+  async getProfile(
+    @Param('username') username: string,
+    @Req() req: any
+  ) {
+    const requesterId = req.user?.id;
+    const result = await this.usersService.getFullProfile(username, requesterId);
+
+    if (!result) return { success: false, error: { message: 'Utilizador não encontrado.' } };
+
+    return { success: true, data: result };
   }
 
   // ── Editar perfil ──────────────────────────────────────────────────
@@ -65,6 +70,7 @@ export class UsersController {
       cb(null, true);
     },
   }))
+
   async uploadAvatar(
     @CurrentUser() user: { id: string },
     @UploadedFile() file: Express.Multer.File,
@@ -118,5 +124,52 @@ export class UsersController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteAccount(@CurrentUser() user: { id: string }) {
     await this.usersService.softDeleteUser(user.id);
+    return
   }
+
+  // ── Sistema de Seguimento ───────────────────────────────────────────────────
+
+  // ── Seguir ───────────────────────────────────────────────────
+  @UseGuards(JwtAuthGuard)
+  @Post(':username/follow')
+  async follow(
+    @Param('username') username: string,
+    @Req() req: any
+  ) {
+    const requesterId = req.user.id
+    await this.usersService.follow(requesterId, username)
+    return {
+      success: true,
+      message: `Agora segues ${username}`
+    }
+  }
+
+  // ── Parar de seguir ───────────────────────────────────────────────────
+  @UseGuards(JwtAuthGuard)
+  @Delete(':username/follow')
+  async unfollow(
+    @Param('username') username: string,
+    @Req() req: any
+  ) {
+    const requesterId = req?.user.id
+    await this.usersService.unfollow(requesterId, username)
+    return {
+      success: true,
+      message: `Deixaste de seguir ${username}`
+    }
+  }
+  // ── Lista de seguidores ───────────────────────────────────────────────────
+  @Get(':username/followers')
+  async followers(@Param('username') username: string) {
+    const followers = await this.usersService.getFollowers(username);
+    return { success: true, data: followers }
+
+  }
+  // ── Lista de seguindos ───────────────────────────────────────────────────
+  @Get(':username/followings')
+  async followings(@Param('username') username: string) {
+    const followings = await this.usersService.getFollowings(username)
+    return { success: true, data: followings }
+  }
+
 }
